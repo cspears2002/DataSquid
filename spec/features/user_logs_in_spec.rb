2866e6eb544f65c9386a7d70a81d5bda03e428f3
cspears2002 <cspears2002@yahoo.com>
2013-12-03 17:23:13 -0800

    Working on creating rpsec tests for logins.

--- spec/features/user_logs_in_spec.rb
+++ spec/features/user_logs_in_spec.rb
@@ -5,13 +5,32 @@ describe "the signin process", :type => :feature do
     User.create(:name => 'user@example.com', :password => 'caplin')
   end
 
-  it "signs me in" do
-    visit '/authentications/new'
-    within("#authentication") do
-      fill_in 'Login', :with => 'user@example.com'
-      fill_in 'Password', :with => 'password'
+  feature "user logs in" do
+    context "successfully" do
+      # User logs in with correct credentials.
+      scenario "logs in with correct credentials" do
+        it "signs me in" do
+          visit '/authentications/new'
+          within("#authentication") do
+            fill_in 'Login', :with => 'user@example.com'
+            fill_in 'Password', :with => 'password'
+          end
+          click_link 'Sign in'
+          expect(page).to have_content 'Success'
+        end
+      end
     end
-    click_link 'Sign in'
-    expect(page).to have_content 'Success'
   end
+
+  # User doesn't exist in db.
+  it "creates a new user" do
+  end
+
+  # User exists in db but the wrong password is given.
+  it "will not let me log in" do
+  end
+
+end
+
+describe "the signout process", :type => :feature do
 end
