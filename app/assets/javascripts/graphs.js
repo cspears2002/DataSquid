7ee434e3576364c14bb32f88da6db28c4f52d10f
cspears2002 <cspears2002@yahoo.com>
2013-12-09 22:39:26 -0800

    Caused a small force directed graph to appear.

--- app/assets/javascripts/graphs.js
+++ app/assets/javascripts/graphs.js
@@ -7,54 +7,56 @@ $(function() {
 
 // Make force directed graph
 $(function() {
-  d3.select("#display_graph").style(function() {
-    var width = 960,
-        height = 500;
 
-    var color = d3.scale.category20();
+  // Sets size
+  var width = 960,
+      height = 500;
 
-    var force = d3.layout.force()
-        .charge(-120)
-        .linkDistance(30)
-        .size([width, height]);
+  var color = d3.scale.category20();
 
-    var svg = d3.select("body").append("svg")
-        .attr("width", width)
-        .attr("height", height);
+  // Select the div and append a svg
+  var svg = d3.select("#display_graph").append("svg")
+      .attr("width", width)
+      .attr("height", height);
 
-    var json_from_db = $('.graph_json').data('json');
-    d3.json(json_from_db, function(error, graph) {
-      force
-          .nodes(graph.nodes)
-          .links(graph.links)
-          .start();
+  // Pull json from graph
+  var json_from_db = $('.graph_json').data('json');
+  var json_nodes = json_from_db.nodes
+  var json_links = json_from_db.links
 
-      var link = svg.selectAll(".link")
-          .data(graph.links)
-        .enter().append("line")
-          .attr("class", "link")
-          .style("stroke-width", function(d) { return Math.sqrt(d.value); });
+  // Sets up the force directed graph
+  var force = d3.layout.force()
+      .charge(-120)
+      .linkDistance(30)
+      .size([width, height])
+      .nodes(json_nodes)
+      .links(json_links)
+      .start();
 
-      var node = svg.selectAll(".node")
-          .data(graph.nodes)
-        .enter().append("circle")
-          .attr("class", "node")
-          .attr("r", 5)
-          .style("fill", function(d) { return color(d.group); })
-          .call(force.drag);
+  var link = svg.selectAll(".link")
+      .data(json_links)
+    .enter().append("line")
+      .attr("class", "link")
+      .style("stroke-width", function(d) { return Math.sqrt(d.value); });
 
-      node.append("title")
-          .text(function(d) { return d.name; });
+  var node = svg.selectAll(".node")
+      .data(json_nodes)
+    .enter().append("circle")
+      .attr("class", "node")
+      .attr("r", 5)
+      .style("fill", function(d) { return color(d.group); })
+      .call(force.drag);
 
-      force.on("tick", function() {
-        link.attr("x1", function(d) { return d.source.x; })
-            .attr("y1", function(d) { return d.source.y; })
-            .attr("x2", function(d) { return d.target.x; })
-            .attr("y2", function(d) { return d.target.y; });
+  node.append("title")
+      .text(function(d) { return d.name; });
 
-        node.attr("cx", function(d) { return d.x; })
-            .attr("cy", function(d) { return d.y; });
-      });
-    });
+  force.on("tick", function() {
+    link.attr("x1", function(d) { return d.source.x; })
+        .attr("y1", function(d) { return d.source.y; })
+        .attr("x2", function(d) { return d.target.x; })
+        .attr("y2", function(d) { return d.target.y; });
+
+    node.attr("cx", function(d) { return d.x; })
+        .attr("cy", function(d) { return d.y; });
   });
 });
