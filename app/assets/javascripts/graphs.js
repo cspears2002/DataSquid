d0baa6531e9b9286173831d4b95fd1958d434f35
cspears2002 <cspears2002@yahoo.com>
2013-12-11 15:15:00 -0800

    Clears out the div every time before creating a new graph.

--- app/assets/javascripts/graphs.js
+++ app/assets/javascripts/graphs.js
@@ -1,6 +1,5 @@
 // Grab the user id from rails and load graphs#new
-
-ready = function() {
+var ready = function() {
   var user_id = $('.user_info').data('user-id');
   var route = ['/users', user_id, 'graphs', 'new'].join('/');
   $("#create_new_graph").load(route);
@@ -11,7 +10,14 @@ $(document).ready(ready);
 $(document).on('page:load', ready);
 
 // Make force directed graph
-$(function() {
+$(document).ready( function() {
+  $("#refresh_btn").click(make_graph);
+});
+
+var make_graph = function() {
+
+  // Clear out the div first
+  $("#display_graph svg:first").remove();
 
   // Sets size
   var width = 960,
@@ -31,9 +37,11 @@ $(function() {
   var json_links = json_from_db.links
 
   // Sets up the force directed graph
+  var charge_val = $('#charge').val();
+  var link_distance_val = $('#link_distance').val();
   var force = d3.layout.force()
-      .charge(-120) // user adjusts?
-      .linkDistance(100) // user adjusts?
+      .charge(charge_val)
+      .linkDistance(link_distance_val)
       .size([width, height])
       .nodes(json_nodes)
       .links(json_links)
@@ -51,8 +59,9 @@ $(function() {
       .attr("class", "node")
       .call(force.drag);
 
+  var radius = $('#radius').val();
   node.append("circle")
-      .attr("r", 10) // user adjusts?
+      .attr("r", radius)
       .style("fill", function(d) { return color(d.group); });
       
   node.append("text")
@@ -67,4 +76,4 @@ $(function() {
         .attr("y2", function(d) { return d.target.y; });
     node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
   });
-});
+};
