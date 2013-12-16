4bf6295c9ad5993e5ac9e04e9101db179fdc89d1
cspears2002 <cspears2002@yahoo.com>
2013-12-16 12:13:59 -0800

    Still styling table.

--- app/models/graph.rb
+++ app/models/graph.rb
@@ -6,6 +6,19 @@ class Graph < ActiveRecord::Base
   validates :name, presence: true
   validates :data, presence: true
 
+  def make_uniq_links(file_path, key)
+    parsed_json = self.parse_file(file_path)
+    links = parsed_json["links"]
+
+    just_source = Array.new
+    links.each do |link|
+      just_source.push(link[key])
+    end
+    
+    uniq_links = just_source.uniq.sort
+    uniq_links
+  end
+
   def parse_file(file_path)
     path = Rails.root.join(file_path);
 
