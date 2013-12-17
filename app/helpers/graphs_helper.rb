41ef43e79d363266a1d7c6a76522aec49b9851c2
cspears2002 <cspears2002@yahoo.com>
2013-12-17 10:56:35 -0800

    Trying a more object oriented approach.

--- app/helpers/graphs_helper.rb
+++ app/helpers/graphs_helper.rb
@@ -1,2 +1,9 @@
 module GraphsHelper
+
+  def horizonal_row graph
+    graph.set_graph_size(graph.data).each do |node|
+      node["name"]
+    end
+  end
+
 end
--- app/models/graph.rb
+++ app/models/graph.rb
@@ -2,6 +2,7 @@ require 'json'
 
 class Graph < ActiveRecord::Base
   belongs_to :user
+  has_many :links
 
   validates :name, presence: true
   validates :data, presence: true
@@ -31,35 +32,6 @@ class Graph < ActiveRecord::Base
     nodes = parsed_json["nodes"]
 
     nodes
-  end
-
-  # Determine if a checkbox should be checked
-  def is_checked(file_path, src, target)
-    parsed_json = self.parse_file(file_path)
-    nodes = parsed_json["nodes"]
-
-    # Create a hash of { name => index }
-    names_array = Array.new
-    nodes.each do |node|
-      names_array.push(node["name"])
-    end
-    names_hash = Hash[names_array.map.with_index.to_a]
-
-    # Compare passed in source and target to source/target
-    # pairs in links array
-    src_target_hash = {source: names_hash[src],
-                       target: names_hash[target]}
-    links = parsed_json["links"]
-    links.each do |link|
-      if link["source"] == src_target_hash[:source] and 
-         link["target"] == src_target_hash[:target]
-          is_checked = true
-          break
-      end
-    end
-
-    is_checked
-
   end
   
 end
