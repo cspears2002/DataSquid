7cecc22f54867951fe1ab9946119bb34fbf8bf30
cspears2002 <cspears2002@yahoo.com>
2013-12-18 14:35:00 -0800

    Got checkboxes to be checked properly.

--- app/models/graph.rb
+++ app/models/graph.rb
@@ -1,4 +1,6 @@
 require 'json'
+require 'links.rb'
+require 'nodes.rb'
 
 class Graph < ActiveRecord::Base
   belongs_to :user
@@ -30,8 +32,26 @@ class Graph < ActiveRecord::Base
   def set_graph_size(file_path)
     parsed_json = self.parse_file(file_path)
     nodes = parsed_json["nodes"]
-
     nodes
+  end
+
+
+  def compare_names(name_1, name_2)
+    # Get the links that belong to the graph
+    links = Links.where(graph_id: self)
+
+    # Compare the names to the names in the nodes
+    value = false
+    links.each do |link|
+      src_node = Nodes.find_by(id: link.source_node_id)
+      target_node = Nodes.find_by(id: link.target_node_id)
+      src_name = src_node.name
+      target_name = target_node.name
+      if src_name == name_1 && target_name == name_2
+        value = true
+      end
+    end
+    value
   end
   
 end
